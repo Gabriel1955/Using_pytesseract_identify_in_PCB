833ff24935844055b994303f67bb341b204ce5c9
Gabriel Paulino Farias da Silva <gabrieltrilogia@gmail.com>
2020-02-27 09:10:22 -0300

    add filter mediablur

--- filter.py
+++ filter.py
@@ -1,31 +1,8 @@
-import pytesseract
-from PIL import Image
-import numpy as np
 import cv2
 
+imgInput = cv2.imread("motherboards/placa2.jpg", 0)
+imgInput = cv2.adaptiveThreshold(imgInput, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                 cv2.THRESH_BINARY, 11, -2)
+imgInput = cv2.medianBlur(imgInput, 7)
 
-def correcting_text(text):
-    letters = ['R', 'C', 'P', 'U', 'Q']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-
-img = cv2.imread('c/c2.png')
-(T, img) = cv2.threshold(img, 190, 255, cv2.THRESH_BINARY)
-img = cv2.medianBlur(img, 7)
-text = pytesseract.image_to_string(img, lang='myFont2')
-font = cv2.FONT_HERSHEY_SIMPLEX
-# org
-org = (50, 50)
-# fontScale
-fontScale = 1
-# Blue color in BGR
-color = (255, 0, 0)
-# Line thickness of 2 px
-thickness = 2
-print(text)
-print(len(text))
-img = cv2.putText(img, text, org, font,
-                  fontScale, color, thickness, cv2.LINE_AA)
-cv2.imshow('frame', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite("Outputs_tests_filters/saida_teste2.jpg", imgInput)
